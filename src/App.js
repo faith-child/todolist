98692737b03bcbf71f86f5669979322a07d05272
faith-child <fxchild05@gmail.com>
2020-12-28 21:59:38 +0000

    added style to list

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+
+      <header className="list-header">
+        <h1>My Tasks</h1>
+      </header>
+      <body>
+        <ul className="list">
+          <li>
+            task 1
+          </li>
+          <li>
+            task 2
+          </li>
+          <li>
+            task 3
+          </li>
+        </ul>
+      </body>
+    </div>
+  );
+}
+
+export default App;
